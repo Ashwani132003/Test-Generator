13847d2632f6f72dc86d4e151a3f3ffd65865e94
STAR SIWACH <76273539+Ashwani132003@users.noreply.github.com>
2021-10-03 18:18:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+ function chck() {
+     var str = document.getElementsByName("Option 1").value;
+     if (value == "Option 1") {
+         document.querySelector(".option").style.backgroundColor = "blue";
+     }
+ }
